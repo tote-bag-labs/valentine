1b712b98a0a4b8299d36b676452f92b4c9c5d7ea
Jose Diaz Rohena <strawberrydiaz@gmail.com>
2023-03-25 23:57:49 +0100

    Add a macro for marking variables as unused.
    Using this macro is sometimes a good idea...but I
    decided to not use it in subsequent commits. Adding
    it anyway (the irony).

---
+++ libs/tote_bag/utils/macros.hpp
@@ -0,0 +1,16 @@
+//
+//  macros.hpp
+//  Assets
+//
+//  Created by Jose Diaz on 25.03.23.
+//
+
+#ifndef macros_hpp
+#define macros_hpp
+
+/** Silences "unused parameter" compiler warnings. Ideally, of course, one would remove the unused
+    parameter. However, it is sometimes more expedient to (temporarily) use this macro.
+ */
+#define TBL_Unused(x) (void) (x)
+
+#endif /* macros_hpp */
