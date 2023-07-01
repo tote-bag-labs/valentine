23da051080804da8dd3ef561aa615aed5e4c0627
Jose Diaz Rohena <strawberrydiaz@gmail.com>
2023-07-01 15:51:59 +0200

    Rescale Saturation param display range

--- src/ValentineParameters.h
+++ src/ValentineParameters.h
@@ -128,7 +128,7 @@ inline const std::array<juce::String, numParams>& VParameterUnit()
     static const std::array<juce::String, numParams> unitLabels = {
         "",
         " dB",
-        " %",
+        "",
         "",
         " ms",
         " ms",
@@ -144,7 +144,7 @@ inline const std::array<juce::String, numParams>& VParameterUnit()
 static constexpr std::array<float, numParams> FFCompParameterMin = {
     1.0f,
     -24.0f,
-    0.0f,
+    1.0f,
     kRatioMin,
     0.02f,
     50.0f,
@@ -157,7 +157,7 @@ static constexpr std::array<float, numParams> FFCompParameterMin = {
 static constexpr std::array<float, numParams> FFCompParameterMax = {
     10.0f,
     48.0f,
-    100.0f,
+    10.0f,
     kRatioParameterMax,
     30.0f,
     1100.0f,
@@ -196,7 +196,7 @@ static constexpr std::array<float, numParams> FFCompParameterIncrement = {
 static constexpr std::array<float, numParams> FFCompParamCenter = {
     5.0f,
     23.0f,
-    60.0f,
+    5.0f,
     6.0f,
     5.0f,
     300.0f,
