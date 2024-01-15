e747622282e21850b89f99505704a5b2126337af
Jose Diaz Rohena <strawberrydiaz@gmail.com>
2024-01-15 21:53:28 +0100

    Reorder global colours
    It's a little easier to read this way.

--- libs/tote_bag/juce_gui/lookandfeel/LookAndFeelConstants.h
+++ libs/tote_bag/juce_gui/lookandfeel/LookAndFeelConstants.h
@@ -21,10 +21,9 @@ const juce::Colour valentinePinkDark (0xffe0a2d5);
 const juce::Colour grassGreen (0xff43a028);
 const juce::Colour avocadoGreen (0xff83a028);
 const juce::Colour racecarRed (0xffef202a);
-
+const juce::Colour lightGrey = juce::Colour (125, 125, 125);
+const juce::Colour transparentMediumGrey = juce::Colour (105, 105, 105).withAlpha (0.3f);
 const juce::Colour mediumGrey = juce::Colour (105, 105, 105);
 const juce::Colour slightlyTransparentBlack = juce::Colour (0, 0, 0).withAlpha (0.6f);
-const juce::Colour transparentMediumGrey = juce::Colour (105, 105, 105).withAlpha (0.3f);
-const juce::Colour lightGrey = juce::Colour (125, 125, 125);
 } // colours
 } // tote_bag
