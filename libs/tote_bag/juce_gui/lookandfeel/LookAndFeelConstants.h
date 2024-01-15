363e30d4514100a3d7cf90093e79e4c125632da0
Jose Diaz Rohena <strawberrydiaz@gmail.com>
2024-01-15 21:28:23 +0100

    Remove unused colours

--- libs/tote_bag/juce_gui/lookandfeel/LookAndFeelConstants.h
+++ libs/tote_bag/juce_gui/lookandfeel/LookAndFeelConstants.h
@@ -21,14 +21,10 @@ const juce::Colour vPinkDark (0xffe0a2d5);
 const juce::Colour vGreen1 (0xff43a028);
 const juce::Colour vGreen2 (0xff83a028);
 const juce::Colour vRed (0xffef202a);
-const juce::Colour vYellow (0xffffce00);
 
 const juce::Colour vColour_1 = juce::Colour (105, 105, 105);
-const juce::Colour vColour_2 = juce::Colour (0, 0, 0).withAlpha (0.0f);
-const juce::Colour vColour_3 = juce::Colour (0, 0, 0).withAlpha (0.3f);
 const juce::Colour vColour_4 = juce::Colour (0, 0, 0).withAlpha (0.6f);
 const juce::Colour vColour_5 = juce::Colour (105, 105, 105).withAlpha (0.3f);
-const juce::Colour vColour_6 = juce::Colour (0, 0, 0).withAlpha (0.8f);
 const juce::Colour vColour_7 = juce::Colour (125, 125, 125);
 } // laf_constants
 } // tote_bag
