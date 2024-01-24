bcbe5c80d98956d5f46cb6a7835d74dba30a9f39
Jose Diaz Rohena <strawberrydiaz@gmail.com>
2024-01-24 22:30:05 +0100

    Enable JUCE_LIVE_CONSTANT in LookAndFeel.cpp
    If it is enabled in cmakelists

--- libs/tote_bag/juce_gui/lookandfeel/LookAndFeel.cpp
+++ libs/tote_bag/juce_gui/lookandfeel/LookAndFeel.cpp
@@ -14,6 +14,10 @@
 #include "tote_bag/juce_gui/components/widgets/FlatTextButton.h"
 #include "tote_bag/juce_gui/components/widgets/tbl_ToggleButton.h"
 
+#if JUCE_ENABLE_LIVE_CONSTANT_EDITOR
+    #include <juce_gui_extra/juce_gui_extra.h>
+#endif // JUCE_ENABLE_LIVE_CONSTANT_EDITOR
+
 namespace tote_bag
 {
 
