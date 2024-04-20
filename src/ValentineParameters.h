3cc92c12783a033bae556dde19327b71faa69ce5
Jose Diaz <strawberrydiaz@gmail.com>
2024-04-20 22:42:00 +0200

    Decrease maximum saturation gain

--- src/ValentineParameters.h
+++ src/ValentineParameters.h
@@ -44,7 +44,7 @@ inline constexpr float kMinBits = 1.0f;
 inline constexpr float kMaxBits = 12.0f;
 
 inline constexpr float kMinSaturationGain = 0.5f;
-inline constexpr float kMaxSaturationGain = 31.62f;
+inline constexpr float kMaxSaturationGain = 10.0f;
 
 // The largest the ratio can be as far as the parameter itself is concerned.
 // Processing, we actually use a ratio of 1000:1 for this ratio value.
